ffa2f8941af8efec99260ec7a58d71ca1e89aa88
naiarani <88119031+naiarani@users.noreply.github.com>
2024-04-23 01:02:37 -0500

    Update calico.py

--- app/environments/calico/calico/envs/calico.py
+++ app/environments/calico/calico/envs/calico.py
@@ -35,7 +35,7 @@ class CalicoEnv(gym.Env):
         self.num_squares = self.quilt_size * self.quilt_size
 
         self.action_space = gym.spaces.Discrete(25)
-        self.observation_space = gym.spaces.Box(low=0, high=1, shape=(self.grid_shape[0], self.grid_shape[1], len(self.colors) * len(self.patterns)), dtype=np.float32)
+        self.observation_space = gym.spaces.Box(low=0, high=1, shape=(self.num_squares, self.grid_shape[1], len(self.colors) * len(self.patterns)), dtype=np.float32)
         self.verbose = verbose
 
         self.quilt_boards = [np.zeros(self.board_size, dtype=int) for _ in range(self.n_players)]
